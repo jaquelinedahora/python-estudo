5f9fede2b446480a94a1ce1b13a2f39027144c99
Jaqueline da Hora <82454233+jaquelinedahora@users.noreply.github.com>
2021-09-14 20:52:00 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-# import pyautogui
-# import time
-# import teste.variavel as variavel
 
-teste=20
-print (teste)
-teste=1
-print (teste)
-teste=3
-print (teste)
-teste=7
-print (teste)
+a = int (input("primeiro valor: "))
+b = int (input("segundo valor: "))
+c = int (input("terceiro valor: "))
 
-# pyautogui.moveTo(500, 500, duration=2, tween=pyautogui.easeInOutQuad)
-
-# time.sleep(3)
-# currentMouseX, currentMouseY = pyautogui.position()
-# print(currentMouseX)
-# print(time.localtime())
-# print ('bem vindo')
-# print (variavel.data_de_hoje())
+if a > b and a > c:
+    print("o maior número é {}".format(a))
+elif b > a and b > c:
+    print("o maior número é {}".format(b))
+else:
+    print("o maior numero é {}".format (c))
+print("final do programa")
