2a14d3701f0125f3ea43673b442429b536b26fc9
Jaqueline da Hora <82454233+jaquelinedahora@users.noreply.github.com>
2021-09-14 20:53:27 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+a = int (input("primeiro valor: "))
+b = int (input("segundo valor: "))
+c = int (input("terceiro valor: "))
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if a > b and a > c:
+    print("o maior número é {}".format(a))
+elif b > a and b > c:
+    print("o maior número é {}".format(b))
+else:
+    print("o maior numero é {}".format (c))
+print("final do programa")
