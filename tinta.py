1acaeb9cbabef012531828eef4cfa12e5916e72b
Jaqueline da Hora <82454233+jaquelinedahora@users.noreply.github.com>
2022-09-19 18:02:57 -0300

    Add files via upload

---
+++ tinta.py
@@ -0,0 +1,10 @@
+altura = float(input())
+largura = float(input())
+num_parede = float(input())
+
+tinta = 20
+area_pintada = tinta * 10
+hora = 30
+hora_pintada = hora*10
+print(float(input(altura*largura+num_parede)*area_pintada*hora_pintada))
+
