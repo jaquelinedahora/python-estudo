20ceb7325e20fe366630ce7a898e3b7c3090dd58
Jaqueline da Hora <82454233+jaquelinedahora@users.noreply.github.com>
2022-09-19 19:33:28 -0300

    Add files via upload

--- tinta.py
+++ tinta.py
@@ -2,9 +2,21 @@ altura = float(input())
 largura = float(input())
 num_parede = float(input())
 
-tinta = 20
-area_pintada = tinta * 10
-hora = 30
-hora_pintada = hora*10
-print(float(input(altura*largura+num_parede)*area_pintada*hora_pintada))
+total_m2_obra = altura*largura*num_parede
+
+valor_tinta = 20
+valor_metro_tinta = valor_tinta/5
+
+valor_hora = 30
+valor_hora_pintada = valor_hora/10
+
+hora_trabalho = valor_hora*total_m2_obra
+horastrabalhadas_hora = total_m2_obra / 10
+
+
+custos_tinta = total_m2_obra * valor_metro_tinta
+custo_mao_obra = horastrabalhadas_hora * valor_hora
+
+print(custos_tinta + custo_mao_obra)
+
 
