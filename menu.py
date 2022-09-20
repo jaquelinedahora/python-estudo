aebc184517c4456ae28f866fe39d0a794f9533ac
Jaqueline da Hora <82454233+jaquelinedahora@users.noreply.github.com>
2022-09-20 14:59:25 -0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,12 @@
+print('Menu:\n1 = Escreva Guilherme\n2 = escreve joão\n3 = escreve Maria')
+
+opção = input('escola uma opção: ')
+
+if opção == '1' :
+    print('Guilherme')
+elif opção == '2':
+    print('João')
+elif opção == '3':
+    print('Maria')
+else:
+    print('Opção invalida')
