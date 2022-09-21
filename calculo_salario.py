2d16fbe1be05cdcef3dfeb843527211a06393a9e
Jaqueline da Hora <82454233+jaquelinedahora@users.noreply.github.com>
2022-09-21 19:06:45 -0300

    Add files via upload

---
+++ calculo_salario.py
@@ -0,0 +1,5 @@
+valor_hora_trabalho = float(input('Entre com o valor da hora de trabalho R$: '))
+num_hora_trabalho = float(input('Entre com o nº de horas trabalhadas: '))
+percentual_adicional = 0.10
+
+print ('Total a ser pago R$:', valor_hora_trabalho * num_hora_trabalho * percentual_adicional)
