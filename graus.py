fccd8fdfffe75edc482d9cf059ea1355caac0f07
Jaqueline da Hora <82454233+jaquelinedahora@users.noreply.github.com>
2022-09-21 18:17:30 -0300

    Add files via upload

--- graus.py
+++ graus.py
@@ -3,3 +3,9 @@ Graus_Celsius = float(input('Temperatura em Graus Celsius para conversão Fahren
 Conversao_C_F = (Graus_Celsius * 9 / 5) + 32
 
 print('A conversão ficou em', Conversao_C_F ,'ºF')
+
+Graus_Fahrenheit = float(input('Temperatura em Graus F para conversão Fahrenheit:'))
+
+Conversao_C_F = (Graus_Fahrenheit - 32) * 5/9
+
+print('A conversão ficou em', Conversao_C_F ,'ºC')
