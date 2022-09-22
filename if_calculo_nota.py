e676ada21cd2877094d983bed04ebb59fe77301a
Jaqueline da Hora <82454233+jaquelinedahora@users.noreply.github.com>
2022-09-22 15:09:00 -0300

    Add files via upload

---
+++ if_calculo_nota.py
@@ -0,0 +1,26 @@
+print("********************")
+print("Calculo das Notas:")
+print("********************")
+
+nota_1 = float(input("Nota 1: "))
+if (nota_1 < 0 or nota_1 > 10):
+     print("insira uma nota entre 0 a 10")
+
+
+nota_2 = float(input("Nota 2: "))
+if (nota_2 < 0 or nota_2 > 10):
+     print("insira uma nota entre 0 a 10")
+
+
+soma_notas = (nota_1 + nota_2) / 2
+
+if (soma_notas >=5) and (soma_notas <=10):
+    print("\n Aprovado")
+if (soma_notas <=0) and (soma_notas >=10):
+    print("insira uma nota entre 0 a 10")
+if (soma_notas <=5):
+    print("\n Reprovado")
+
+print("********************")
+print("Dados inválidos, reinicie a operação!")
+print("********************")
