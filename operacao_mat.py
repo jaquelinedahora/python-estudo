4d771172b65929f3486cc168d1c6a75580800bde
Jaqueline da Hora <82454233+jaquelinedahora@users.noreply.github.com>
2023-11-04 16:49:03 -0300

    Add files via upload

---
+++ operacao_mat.py
@@ -0,0 +1,10 @@
+# Escreva um programa que pede que o usuário de entrada em dois valores, em seguida
+# exiba em tela o resulta da soma, subtracao, multiplicacao e divisao desses numeros
+
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+print(f'A soma de {num1} com {num2} é: {num1 + num2}')
+print(f'A divisão de {num1} com {num2} é: {num1 / num2}')
+print(f'A multiplicação de {num1} com {num2} é: {num1 * num2}')
